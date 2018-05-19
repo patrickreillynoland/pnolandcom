9f8302f4c8085b74b7c19a90461e941bb7b6ade6
patrickreillynoland <patrickreillynoland@gmail.com>
2018-05-18 20:09:12 -0400

    include EJS dependency and create views

--- server/index.js
+++ server/index.js
@@ -6,5 +6,9 @@ var clientPath = path.join(__dirname, '../client');
 var app = express();
 
 app.use(express.static(clientPath));
-
+app.set('views', path.join(__dirname, '..'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
 app.listen(process.env.PORT || 3000);
+
+  
