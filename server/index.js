47c424a751d1d88987b89519c40b7af8f7cad37b
patrickreillynoland <patrickreillynoland@gmail.com>
2018-05-18 21:42:12 -0400

    It definitely works this time, right

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const path = require('path');
 const express = require ('express');
 
-var clientPath = path.join(__dirname, '../client');
+var clientPath = path.join(__dirname, '..');
 
 var app = express();
 
-app.use(express.static(clientPath));
-app.set('views', path.join(__dirname, '..'));
-app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('pages/index'));
-app.listen(process.env.PORT || 3000);
+app
+    .use(express.static(clientPath))
+    .set('views', path.join(__dirname, '../views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('index'))
+    .listen(process.env.PORT || 3000, () => console.log(`Listening on ${PORT}`));
 
   
