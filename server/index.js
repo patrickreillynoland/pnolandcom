70e61d63c98173aba14417f352dee0f1439dee45
patrickreillynoland <patrickreillynoland@gmail.com>
2018-05-31 17:01:33 -0400

    Force HTTPS

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require ('express');
+const secure = require('ssl-express-www');
 
 const clientPath = path.join(__dirname, '..');
 
 const app = express();
 
 app
+    .use(secure)
     .use(express.static(clientPath))
     .set('views', path.join(__dirname, '../views'))
     .set('view engine', 'ejs')
