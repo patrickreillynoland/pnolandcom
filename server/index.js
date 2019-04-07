1cd422ff255cefe825cc765edf0e03d833adb61f
patrickreillynoland <patrickreillynoland@gmail.com>
2019-04-07 17:04:00 -0400

    force HTTPS

--- server/index.js
+++ server/index.js
@@ -6,8 +6,17 @@ const clientPath = path.join(__dirname, '..');
 
 const app = express();
 
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+      return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+  }
+
 app
     .use(express.static(clientPath))
+    .use(requireHTTPS)
     .set('views', path.join(__dirname, '../views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('index'))
